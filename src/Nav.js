80f3b344331d12431648f55cd8a5fbc22192fa36
Anurag Deegwal <anurag@Anurags-Air.mshome.net>
2022-08-14 00:27:53 +0530

    initial commit

---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleshow] = useState(false);
+
+  useEffect(() => {
+    const srija = () => {
+      if (window.scrollY > 100) {
+        handleshow(true);
+      } else handleshow(false);
+    };
+
+    
+    window.addEventListener("scroll", srija);
+    return () => {
+      window.removeEventListener("scroll", srija);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
